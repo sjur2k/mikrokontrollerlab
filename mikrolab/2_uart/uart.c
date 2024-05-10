a9b9d2916c6584a8de869b5876f7473e08641386
EvenFosby <even.fosby@gmail.com>
2024-05-10 12:36:34 +0200

    h

--- mikrolab/2_uart/uart.c
+++ mikrolab/2_uart/uart.c
@@ -3,7 +3,21 @@
 #define GPIO ((NRF_GPIO_REG*)0x50000000)
 #define UART ((NRF_UART_REG*)0x40002000)
 
-void uart_init(){
+typedef struct {
+    volatile uint32_t RESERVED0[128];
+    volatile uint32_t SHORTS;
+    volatile uint32_t RESERVED1[191];
+    volatile uint32_t ENABLE;
+    volatile uint32_t PSELRTS;
+    volatile uint32_t PSELTXD;
+    volatile uint32_t PSELCTS;
+    volatile uint32_t PSELRXD;
+    volatile uint32_t RESERVED2[3];
+    volatile uint32_t BAUDRATE;
+} NRF_UART_REG;
+
+
+/*void uart_init(){
     
     GPIO->PIN_CNF[8] = (0<<0);
     GPIO->PIN_CNF[6] = (1<<0);
@@ -15,9 +29,10 @@ void uart_init(){
     UART->ENABLE=4;
     UART->SHORTS=1<<3; //?
     return;
-}
+}*/
 void uart_send(char letter){
     return;
 }
-char uart_read();
-
+char uart_read(){
+    return;
+};
--- mikrolab/2_uart/uart.h
+++ mikrolab/2_uart/uart.h
@@ -5,15 +5,4 @@ void uart_init();
 void uart_send(char letter);
 char uart_read();
 
-typedef struct {
-    volatile uint32_t RESERVED0[128];
-    volatile uint32_t SHORTS;
-    volatile uint32_t RESERVED1[191];
-    volatile uint32_t ENABLE;
-    volatile uint32_t PSELRTS;
-    volatile uint32_t PSELTXD;
-    volatile uint32_t PSELCTS;
-    volatile uint32_t PSELRXD;
-    volatile uint32_t RESERVED2[3];
-    volatile uint32_t BAUDRATE;
-} NRF_UART_REG;
+
